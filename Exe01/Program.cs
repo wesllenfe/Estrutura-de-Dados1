45e08e220d59d785f4a85e8762951be55f76d06e
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2023-08-30 20:32:46 -0300

    Primeiro commit

---
+++ Exe01/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numeros = new int[10];
+
+Random random = new Random();
+for (int i = 0; i < numeros.Length; i++)
+{
+    numeros[i] = random.Next(1, 100);
+}
+
+Console.WriteLine("Vetor desordenado:");
+ImprimirVetor(numeros);
+
+Array.Sort(numeros);
+
+Console.WriteLine("\nVetor ordenado:");
+ImprimirVetor(numeros);
+
+static void ImprimirVetor(int[] numeros)
+{
+    foreach (int valor in numeros)
+    {
+        Console.Write(valor + " ");
+    }
+    Console.WriteLine();
+}
