8fb6781c6080ae380b440cc80f87b2e0ee2177db
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2023-08-30 21:58:36 -0300

    Aula sobre arrays multidimensionais

---
+++ Atividades/MultiDimensionalArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,] biNumbers = new int[5, 2]; //vetor bidimensional
+
+int[,,] triNumbers = new int[5, 4, 3];
+
+int[,] loadedBiNumbers = new int[,]
+{
+    {  9,   5, 8},
+    { 10, -11, 4},
+    {101,  45, -9}
+};
+
+for (int i = 0; i < loadedBiNumbers.GetLength(0); i++)
+{
+    for (int j = 0; j < loadedBiNumbers.GetLength(1); j++)
+    {
+        Console.Write(
+            $"[{loadedBiNumbers[i, j]}]"
+            );
+    }
+    Console.WriteLine("");
+}
