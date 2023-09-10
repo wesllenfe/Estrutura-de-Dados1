437df977a003585693de9587c543247551f6773a
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2023-09-10 12:03:03 -0300

    atividade de fixação pilha

---
+++ Exercicios/exe004/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using exe004;
+
+Stack pilha = new Stack();
+
+do
+{
+    Console.WriteLine("Informe o valor para a pilha:");
+    int valor = Convert.ToInt32(Console.ReadLine());
+    pilha.Push(valor);
+
+    Console.WriteLine("Deseja informar um novo valor?");
+    Console.WriteLine("1 - Sim");
+    Console.WriteLine("2 - Não");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    if (x != 1)
+    {
+        break;
+    }
+
+} while (true);
+
+pilha.PrintStack();
