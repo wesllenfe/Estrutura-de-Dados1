bea8f1eb7e6142f80f57ac41f9c8f047d3d75ddc
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2023-09-25 14:04:05 -0300

    Tabela Hash interativa

---
+++ Exercicios/exe006/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+Hashtable cpfNome = new Hashtable();
+
+while (true)
+{
+    Console.Write("Digite o CPF: ");
+    string cpf = Console.ReadLine();
+
+    Console.Write("Digite o Nome: ");
+    string nome = Console.ReadLine();
+
+    try
+    {
+        if (cpfNome.ContainsKey(cpf))
+        {
+            Console.WriteLine("CPF já inserido previamente.");
+        }
+        else
+        {
+            cpfNome.Add(cpf, nome);
+            Console.WriteLine("CPF e Nome adicionados com sucesso.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro ao adicionar CPF e Nome: {ex.Message}");
+    }
+
+    Console.Write("Deseja continuar (S/N)? ");
+    string continuar = Console.ReadLine();
+
+    if (continuar.ToUpper() != "S")
+    {
+        break;
+    }
+}
+
+Console.WriteLine("\nDados inseridos:");
+foreach (DictionaryEntry de in cpfNome)
+{
+    Console.WriteLine($"CPF: {de.Key} - Nome: {de.Value}");
+}
