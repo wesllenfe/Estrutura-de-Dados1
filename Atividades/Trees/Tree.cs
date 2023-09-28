df8966a78559bf1ddc075b1ad41334cc2cc03374
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2023-09-27 22:06:47 -0300

    Aula sobre 'Variants of Trees'

---
+++ Atividades/Trees/Tree.cs
@@ -0,0 +1,7 @@
+namespace Trees
+{
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+    }
+}
---
+++ Atividades/Trees/TreeNode.cs
@@ -0,0 +1,26 @@
+namespace Trees
+{
+    public class TreeNode<T>
+    {
+        public T Data { get; set; }
+        // atributo recursivo
+        public TreeNode<T> Parent { get; set; }
+        // lista com chamda de tipo recursivo
+        public List<TreeNode<T>> Children { get; set; }
+
+        public int GetHeight()
+        {
+            int height = 1;
+
+            TreeNode<T> current = this;
+            while (current.Parent != null)
+            {
+                height++;
+
+                current = current.Parent;
+            }
+
+            return height;
+        }
+    }
+}
